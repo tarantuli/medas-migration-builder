8b99f8b282304e1473f24ed781edd35ae85432f7
Patrick Morpey <patrick@morphp.nl>
2026-06-15 12:39:24 +0200

    Update dependencies to latest versions and refactor `ServiceManager` bootstrap usage.

--- phpunit.bootstrap.php
+++ phpunit.bootstrap.php
@@ -4,12 +4,12 @@ declare(strict_types=1);
 
 use Medas\ObjectInstantiator\ObjectInstantiator;
 use Medas\MigrationBuilder\MigrationBuilderPackage;
-use Medas\ServiceManager\{ServiceConfig, ServiceManager};
+use Medas\ServiceManager\{ServiceConfigBuilder, ServiceManager};
 
 chdir(__DIR__);
 
 new ServiceManager(function (): ServiceConfig {
-    $config = new ServiceConfig(ObjectInstantiator::class);
+    $config = new ServiceConfigBuilder(ObjectInstantiator::class);
 
     $config->addPackages([
         MigrationBuilderPackage::instance(),
